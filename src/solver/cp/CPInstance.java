857b2e86df9bf1785a4872af3d1ae947afeabd91
zling1 <zling1@cs.brown.edu>
2021-03-03 17:25:42 -0500

    dist vs count exp

--- src/solver/cp/CPInstance.java
+++ src/solver/cp/CPInstance.java
@@ -1,27 +1,22 @@
 package solver.cp;
-
 import ilog.cp.*;
-
 import ilog.concert.*;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-
 import java.util.Scanner;
-
 import java.util.Arrays;
 
 public class CPInstance
 {
   // BUSINESS parameters
   int numWeeks;
-  int numDays;  
+  int numDays;
   int numEmployees;
   int numShifts;
   int numIntervalsInDay;
   int[][] minDemandDayShift;
   int minDailyOperation;
-  
+
   // EMPLOYEE parameters
   int minConsecutiveWork;
   int maxDailyWork;
@@ -35,7 +30,7 @@ public class CPInstance
 
   // ILOG CP Solver
   IloCP cp;
-    
+
   public CPInstance(String fileName)
   {
     try
@@ -195,7 +190,7 @@ public class CPInstance
       cp.setParameter(IloCP.IntParam.Workers, 1);
       cp.setParameter(IloCP.DoubleParam.TimeLimit, 300);
       cp.setParameter(IloCP.IntParam.SearchType, IloCP.ParameterValues.DepthFirst);
-  
+
       // Uncomment this: to set the solver output level if you wish
       cp.setParameter(IloCP.IntParam.LogVerbosity, IloCP.ParameterValues.Quiet);
 
@@ -209,13 +204,24 @@ public class CPInstance
           assignments[day][employee] = cp.intVar(0, numShifts - 1);
         }
       }
-      
-      // there is a certain minimum demand that needs to be met on the number of employees needed every day for every shift
+
+      //there is a certain minimum demand that needs to be met on the number of employees needed every day for every shift
+      // for (int day = 0; day < numDays; day++) {
+      //   for (int shift = 0; shift < numShifts; shift++) {
+      //     int demand = minDemandDayShift[day][shift];
+      //     cp.add(cp.ge(cp.count(assignments[day], shift), demand));
+      //   }
+      // }
+
+      IloIntVar[][] mindDemands = new IloIntVar[numDays][numShifts];
+      int[] counts = new int[]{0, 1, 2, 3};
       for (int day = 0; day < numDays; day++) {
         for (int shift = 0; shift < numShifts; shift++) {
-          int demand = minDemandDayShift[day][shift];
-          cp.add(cp.ge(cp.count(assignments[day], shift), demand));
+          mindDemands[day][shift] = cp.intVar(minDemandDayShift[day][shift], numEmployees);
         }
+      }
+      for (int day = 0; day < numDays; day++) {
+        cp.add(cp.distribute(mindDemands[day], counts, assignments[day]));
       }
 
       // the first 4 days of the schedule is treated specially where employees are assigned to unique shifts.
@@ -320,7 +326,7 @@ public class CPInstance
           for (int day = 0; day < numDays; day++) {
             solvedAssignments[employee][day] = (int)cp.getValue(assignments[day][employee]);
           }
-        }          
+        }
 
         int[][] solvedHours = new int[numEmployees][numDays];
         for (int employee = 0; employee < numEmployees; employee++) {
@@ -388,14 +394,14 @@ public class CPInstance
   }
 
   // SK: technically speaking, the model with the global constaints
-  // should result in fewer number of fails. In this case, the problem 
-  // is so simple that, the solver is able to re-transform the model 
+  // should result in fewer number of fails. In this case, the problem
+  // is so simple that, the solver is able to re-transform the model
   // and replace inequalities with the global all different constrains.
   // Therefore, the results don't really differ
   void solveAustraliaGlobal()
   {
     String[] Colors = {"red", "green", "blue"};
-    try 
+    try
     {
       cp = new IloCP();
       IloIntVar WesternAustralia = cp.intVar(0, 3);
@@ -404,38 +410,38 @@ public class CPInstance
       IloIntVar Queensland = cp.intVar(0, 3);
       IloIntVar NewSouthWales = cp.intVar(0, 3);
       IloIntVar Victoria = cp.intVar(0, 3);
-      
+
       IloIntExpr[] clique1 = new IloIntExpr[3];
       clique1[0] = WesternAustralia;
       clique1[1] = NorthernTerritory;
       clique1[2] = SouthAustralia;
-      
+
       IloIntExpr[] clique2 = new IloIntExpr[3];
       clique2[0] = Queensland;
       clique2[1] = NorthernTerritory;
       clique2[2] = SouthAustralia;
-      
+
       IloIntExpr[] clique3 = new IloIntExpr[3];
       clique3[0] = Queensland;
       clique3[1] = NewSouthWales;
       clique3[2] = SouthAustralia;
-      
+
       IloIntExpr[] clique4 = new IloIntExpr[3];
       clique4[0] = Queensland;
       clique4[1] = Victoria;
       clique4[2] = SouthAustralia;
-      
+
       cp.add(cp.allDiff(clique1));
       cp.add(cp.allDiff(clique2));
       cp.add(cp.allDiff(clique3));
       cp.add(cp.allDiff(clique4));
-      
+
 	  cp.setParameter(IloCP.IntParam.Workers, 1);
       cp.setParameter(IloCP.DoubleParam.TimeLimit, 300);
-	  cp.setParameter(IloCP.IntParam.SearchType, IloCP.ParameterValues.DepthFirst);   
-	  
+	  cp.setParameter(IloCP.IntParam.SearchType, IloCP.ParameterValues.DepthFirst);
+
       if (cp.solve())
-      {    
+      {
          System.out.println();
          System.out.println( "WesternAustralia:    " + Colors[(int)cp.getValue(WesternAustralia)]);
          System.out.println( "NorthernTerritory:   " + Colors[(int)cp.getValue(NorthernTerritory)]);
@@ -448,16 +454,16 @@ public class CPInstance
       {
         System.out.println("No Solution found!");
       }
-    } catch (IloException e) 
+    } catch (IloException e)
     {
       System.out.println("Error: " + e);
     }
   }
-  
+
   void solveAustraliaBinary()
   {
     String[] Colors = {"red", "green", "blue"};
-    try 
+    try
     {
       cp = new IloCP();
       IloIntVar WesternAustralia = cp.intVar(0, 3);
@@ -466,23 +472,23 @@ public class CPInstance
       IloIntVar Queensland = cp.intVar(0, 3);
       IloIntVar NewSouthWales = cp.intVar(0, 3);
       IloIntVar Victoria = cp.intVar(0, 3);
-      
-      cp.add(cp.neq(WesternAustralia , NorthernTerritory)); 
-      cp.add(cp.neq(WesternAustralia , SouthAustralia)); 
+
+      cp.add(cp.neq(WesternAustralia , NorthernTerritory));
+      cp.add(cp.neq(WesternAustralia , SouthAustralia));
       cp.add(cp.neq(NorthernTerritory , SouthAustralia));
       cp.add(cp.neq(NorthernTerritory , Queensland));
-      cp.add(cp.neq(SouthAustralia , Queensland)); 
-      cp.add(cp.neq(SouthAustralia , NewSouthWales)); 
-      cp.add(cp.neq(SouthAustralia , Victoria)); 
+      cp.add(cp.neq(SouthAustralia , Queensland));
+      cp.add(cp.neq(SouthAustralia , NewSouthWales));
+      cp.add(cp.neq(SouthAustralia , Victoria));
       cp.add(cp.neq(Queensland , NewSouthWales));
-      cp.add(cp.neq(NewSouthWales , Victoria)); 
-      
+      cp.add(cp.neq(NewSouthWales , Victoria));
+
 	  cp.setParameter(IloCP.IntParam.Workers, 1);
       cp.setParameter(IloCP.DoubleParam.TimeLimit, 300);
-	  cp.setParameter(IloCP.IntParam.SearchType, IloCP.ParameterValues.DepthFirst);   
-	  
+	  cp.setParameter(IloCP.IntParam.SearchType, IloCP.ParameterValues.DepthFirst);
+
       if (cp.solve())
-      {    
+      {
          System.out.println();
          System.out.println( "WesternAustralia:    " + Colors[(int)cp.getValue(WesternAustralia)]);
          System.out.println( "NorthernTerritory:   " + Colors[(int)cp.getValue(NorthernTerritory)]);
@@ -495,7 +501,7 @@ public class CPInstance
       {
         System.out.println("No Solution found!");
       }
-    } catch (IloException e) 
+    } catch (IloException e)
     {
       System.out.println("Error: " + e);
     }
@@ -503,11 +509,11 @@ public class CPInstance
 
   void solveSendMoreMoney()
   {
-    try 
+    try
     {
       // CP Solver
       cp = new IloCP();
-	
+
       // SEND MORE MONEY
       IloIntVar S = cp.intVar(1, 9);
       IloIntVar E = cp.intVar(0, 9);
@@ -517,20 +523,20 @@ public class CPInstance
       IloIntVar O = cp.intVar(0, 9);
       IloIntVar R = cp.intVar(0, 9);
       IloIntVar Y = cp.intVar(0, 9);
-      
+
       IloIntVar[] vars = new IloIntVar[]{S, E, N, D, M, O, R, Y};
       cp.add(cp.allDiff(vars));
-      
-      //                1000 * S + 100 * E + 10 * N + D 
+
+      //                1000 * S + 100 * E + 10 * N + D
       //              + 1000 * M + 100 * O + 10 * R + E
-      //  = 10000 * M + 1000 * O + 100 * N + 10 * E + Y 
-      
+      //  = 10000 * M + 1000 * O + 100 * N + 10 * E + Y
+
       IloIntExpr SEND = cp.sum(cp.prod(1000, S), cp.sum(cp.prod(100, E), cp.sum(cp.prod(10, N), D)));
       IloIntExpr MORE   = cp.sum(cp.prod(1000, M), cp.sum(cp.prod(100, O), cp.sum(cp.prod(10,R), E)));
       IloIntExpr MONEY  = cp.sum(cp.prod(10000, M), cp.sum(cp.prod(1000, O), cp.sum(cp.prod(100, N), cp.sum(cp.prod(10,E), Y))));
-      
+
       cp.add(cp.eq(MONEY, cp.sum(SEND, MORE)));
-      
+
       // Solver parameters
       cp.setParameter(IloCP.IntParam.Workers, 1);
       cp.setParameter(IloCP.IntParam.SearchType, IloCP.ParameterValues.DepthFirst);
@@ -544,24 +550,24 @@ public class CPInstance
       {
         System.out.println("No Solution!");
       }
-    } catch (IloException e) 
+    } catch (IloException e)
     {
       System.out.println("Error: " + e);
     }
   }
-  
+
  /**
    * Poor man's Gantt chart.
    * author: skadiogl
    *
-   * Displays the employee schedules on the command line. 
-   * Each row corresponds to a single employee. 
+   * Displays the employee schedules on the command line.
+   * Each row corresponds to a single employee.
    * A "+" refers to a working hour and "." means no work
    * The shifts are separated with a "|"
    * The days are separated with "||"
-   * 
-   * This might help you analyze your solutions. 
-   * 
+   *
+   * This might help you analyze your solutions.
+   *
    * @param numEmployees the number of employees
    * @param numDays the number of days
    * @param beginED int[e][d] the hour employee e begins work on day d, -1 if not working
